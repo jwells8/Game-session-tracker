33c4756b72d26ef2ad9e30b38259d32e6e2b41f1
Jefferson Wells <jeff.t.wells@icloud.com>
2026-03-11 16:08:41 -0400

    Create tables for games and sessions in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,19 @@
 #Database setup
+
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS games;
+
+CREATE TABLE games (
+  game_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  game_title VARCHAR(50) NOT NULL UNIQUE,
+  game_genre VARCHAR(20),
+  game_platform VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE sessions (
+  session_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  game_id INTEGER NOT NULL,
+  FOREIGN KEY (game_id) REFERENCES games(game_id),
+  session_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  session_duration INTEGER
+);
