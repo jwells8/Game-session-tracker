050ed03bd198e51d0ab3e761eada68ad73143009
Jefferson Wells <jeff.t.wells@icloud.com>
2026-03-11 18:00:06 -0400

    Initialize SQLite connection for game_tracker.db
    Add database connection setup for game tracking

--- stats.py
+++ stats.py
@@ -1,1 +1,14 @@
 #Script to view my stats
+
+import sqlite3
+
+#Conncect to database
+connection = sqlite3.connect('game_tracker.db')
+cursor = connection.cursor()
+
+
+
+
+#Commit and Close
+connection.commit()
+connection.close()
