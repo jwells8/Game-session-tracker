6baa5138b9e676f5c6e21c7ed3f79d1e842b3576
Jefferson Wells <jeff.t.wells@icloud.com>
2026-03-11 18:31:27 -0400

    Clean up queries.sql by removing comments
    Removed unused comments and cleaned up query file.

--- queries.sql
+++ queries.sql
@@ -1,5 +1,3 @@
---Useful pre-written queries
-
 --Retrieve all sessions with the game title
 SELECT games.game_title, sessions.session_timestamp, sessions.session_duration
 FROM games
@@ -18,16 +16,3 @@ FROM games
 JOIN sessions ON games.game_id = sessions.game_id
 GROUP BY game_title
 ORDER BY total_sessions DESC
-  
---Your most played game by total duration
-  
---Your longest single session ever
-  
---All sessions from the last 30 days
-  
---Average session length per game
-  
---Total hours played per platform
-  
---Which month had the most play time
-
