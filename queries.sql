749627c272889fd637bec0cee7565c37b3fba5e0
Jefferson Wells <jeff.t.wells@icloud.com>
2026-03-11 18:46:25 -0400

    Improve comments in queries.sql
    Updated comments for clarity in SQL queries.

--- queries.sql
+++ queries.sql
@@ -1,16 +1,16 @@
---Retrieve all sessions with the game title
+--retrieve all sessions with the game title
 SELECT games.game_title, sessions.session_timestamp, sessions.session_duration
 FROM games
 JOIN sessions ON games.game_id = sessions.game_id
 
---Total time played per game (in minutes)
+--total time played per game (in minutes)
 SELECT game_title, SUM(session_duration) AS total_minutes
 FROM games
 JOIN sessions ON games.game_id = sessions.game_id
 GROUP BY game_title
 ORDER BY total_minutes DESC
   
---Total sessions per game
+--total sessions per game
 SELECT game_title, COUNT(session_id) AS total_sessions
 FROM games
 JOIN sessions ON games.game_id = sessions.game_id
